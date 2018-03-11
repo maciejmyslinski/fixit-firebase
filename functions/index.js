530ad18243b174b9acd15306c51b4b99f90de535
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-03-12 00:51:22 +0100

    Save every new user to the database

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.addUserToFirestore = functions.auth
+  .user()
+  .onCreate(({ data: { displayName, email, photoURL, uid } }) => {
+    return admin
+      .firestore()
+      .collection('users')
+      .doc(uid)
+      .set({
+        displayName,
+        email,
+        photoURL
+      });
+  });
